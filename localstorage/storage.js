abf8060c83a71bc36ddf615c19de1c90224eb470
Manuel Camacho <maedca@gmail.com>
2014-12-17 18:07:53 -0430

    todo
    too

---
+++ localstorage/storage.js
@@ -0,0 +1,42 @@
+function lista(clave){
+    this.clave = clave ;
+    
+    this.agregarActividad = function(actividad){
+        if(this.validarNavegador()){
+            
+            if(localStorage.getItem(this.clave)!="undefined"){
+                var actividades =JSON.parse(localStorage.getItem(this.clave));
+            }else{
+                var actividades= [];
+            }
+            actividades.push(actividad);
+            localStorage.setItem(this.clave, JSON.stringify(actividades));
+            return true;
+        }
+        return false;
+    }
+    
+    this.obtenerActividades = function(){
+        if(localStorage.getItem(this.clave) !="undefined"){
+            return JSON.parse(localStorage.getItem(this.clave));
+        }
+        return null;
+        
+        this,eliminarActividad = function(actividad){
+            var actividades = JSON.parse(localStorage.getItem(this.clave));
+            actividades = actividades.filter(function(i){
+                return i !=actividad;
+                                             }
+        });
+            localStorage.setItem(this.clave, JSON.stringify(actividades));
+        
+    }
+        this.eliminarActividad=function(){
+            localStorage.removeItem(this.clave);
+        }
+        
+        this.validarNavegador = function(){
+            if(typeof(Storage) !="undefined")   return true;
+            return false;
+        }
+}
